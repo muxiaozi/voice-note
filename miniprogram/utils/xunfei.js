da4f877e32809725b8dbc7bff9ca0cf1eac2ca4b
muxiaozi <1002042998@qq.com>
2019-05-19 12:47:02 +0800

    first commit

---
+++ miniprogram/utils/xunfei.js
@@ -0,0 +1,32 @@
+const app = getApp()
+
+/**
+ * 语音识别
+ * @param filepath 语音文件路径
+ */
+exports.iat = function(filePath){
+  return new Promise((resolve, reject) => {
+    wx.uploadFile({
+      url: 'https://api.muxiaozi.cn/voice/iat',
+      filePath,
+      name: 'file',
+      success: resolve,
+      fail: reject
+    })
+  })
+}
+
+/**
+ * 语音合成
+ * @param content 合成内容
+ */
+exports.tts = function(content){
+  return new Promise((resolve, reject) => {
+    wx.downloadFile({
+      url: 'https://api.muxiaozi.cn/voice/tts?content=' + content,
+      header: {},
+      success: res => resolve(res.tempFilePath),
+      fail: reject
+    })
+  })
+}
